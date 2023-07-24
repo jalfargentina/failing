8f370a1350ccce14dd213431eccde787501e8fcc
Sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-07-24 18:53:24 -0300

    Chat gpt base implementation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import qrcode from 'qrcode-terminal'
 
 // Whatsapp client js
 import {Client, LocalAuth} from 'whatsapp-web.js'
+import { askChatGpt } from './services/chatGPT'
 const client = new Client({
     authStrategy:new LocalAuth({clientId:'client-one'}),
     puppeteer: {
@@ -18,8 +19,15 @@ client.on('ready',()=>{
     console.log('client is ready')
 })
 
-client.on('message',()=>{
-    console.log('holaaaa')
+client.on('message',async msg=>{
+
+   
+        const ask = await askChatGpt(msg.body,'Eres un vendedor de autos, ayudas a la gente con respuestas cortas y concisas. Si la situacion lo permite, usas emojis al final de las oraciones. Te llamas Bob')
+
+        msg.reply(ask)
+  
+
+
 })
 
 client.initialize()
---
+++ src/services/chatGPT.ts
@@ -0,0 +1,16 @@
+import Client from '../client/openai'
+
+const askChatGpt = async (message:string,prompt:string)=>{
+
+    const completion = await Client.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: prompt },
+          { role: 'user', content: message },
+        ],
+    })
+
+    return completion.data.choices[0].message.content
+}
+
+export {askChatGpt}
